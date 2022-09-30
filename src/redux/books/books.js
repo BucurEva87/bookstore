ee8cf401e0697579d81e6090bff7d96c0168a13a
BucurEva87 <bucur.eva87@gmail.com>
2022-09-30 12:46:56 +0300

    Add redux folder structure

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,1 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
+
+export default createSlice({
+  name: 'books',
+  initialState: {
+    isLoading: false,
+    books: [],
+    error: '',
+  },
+});
